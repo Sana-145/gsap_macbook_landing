06b7acb3422ecfc7e52732b3835ab3d4077afb0f
Sana-145 <sanasiddiqua575@gmail.com>
2026-01-20 16:28:20 +0530

    Add SRGB color space

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -28,8 +28,7 @@ const Showcase = () => {
             <div className="media">
                 <video src="/videos/game.mp4" loop muted autoPlay playsInline/>
                 <div className="mask">
-                    <img src="/mask-logo.svg"/>
-                </div>
+                    <img src="/mask-logo.svg" alt="Company logo"/>                </div>
             </div>
 
             <div className="content">
@@ -39,22 +38,21 @@ const Showcase = () => {
 
                         <div className="space-y-5 mt-7 pe-10">
                             <p>
-                                Introducing {" "}
+                                Introducing{" "}
                                 <span className="text-white">
                                     M4, the next generation of Apple silicon
                                 </span>
-                                . M4 powers
+                                .
                             </p>
                             <p>
-                                It drives Apple intelligence on iPad Pro,so you can write,create and accomplish more
+                                It drives Apple Intelligence on iPad Pro, so you can write, create, and accomplish more
                                 with ease.All in a design that's unbelievably thin,light and powerful.
                             </p>
                             <p>
-                                A brand-new display engine delivers breathtaking precision,color accuracy, and
+                                A brand-new display engine delivers breathtaking precision, color accuracy, and
                                 brightness.And a next-gen GPU with hardware-accelerated ray tracing brings console-level
                                 graphics to your fingertips.
-                            </p>
-                            <p className="text-primary">Learn more about Apple Intelligence</p>
+                            </p>                            <p className="text-primary">Learn more about Apple Intelligence</p>
                         </div>
                     </div>
                     <div className="max-w-3xs space-y-14">
